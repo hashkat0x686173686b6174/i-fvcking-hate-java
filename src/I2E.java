7fb8677da061d926b8849e2c760758b47f809526
Léon Kiefer <leon.kiefer@proton.me>
2024-01-08 11:30:10 +0100

    Implement exercise `I2E`

---
+++ src/I2E.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class I2E {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int executionCount = 0;
+
+        while (true) {
+            System.out.print("Number of bottles? (Enter 0 to exit): ");
+            int bottles = scanner.nextInt();
+            if (bottles == 0) {
+                System.out.println("Program exited.");
+                break;
+            }
+
+            int crates = calculateCrates(bottles);
+            System.out.println("You need " + crates + " crate" + (crates != 1 ? "s" : "") + "!");
+
+            // Task b condition
+            if (bottles == 99 || crates == 0) {
+                break;
+            }
+
+            // Task c condition
+            executionCount++;
+            if (executionCount == 3) {
+                System.out.println("Program has been executed 3 times.");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static int calculateCrates(int bottles) {
+        int crates = 0;
+        while (bottles > 0) {
+            bottles -= 6;
+            crates++;
+        }
+        return crates;
+    }
+}
