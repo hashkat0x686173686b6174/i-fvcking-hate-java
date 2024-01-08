d7b057ab35ccbf9aa12ce8229b2b02e0e32a389c
Léon Kiefer <leon.kiefer@proton.me>
2024-01-08 11:00:02 +0100

    Add `E1B` exercise

---
+++ src/E1B.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class E1B {
+        public static void main(String[] args) {
+            // include scanner for reading input
+            Scanner scanner = new Scanner(System.in);
+
+            int ein; // declare variable
+
+            System.out.println("Geben Sie eine Zahl zwischen 3 und 6 ein:");
+
+            ein = scanner.nextInt();
+
+            if (ein >= 3 && ein <= 6) {
+                for (int hor = 1; hor <= ein; hor++) {
+                    String out = "";
+                    for (int vert = hor; vert <= ein; vert++) {
+                        out += "*";
+                    }
+                    System.out.println(out);
+                }
+            } else {
+                System.err.println("Achtung, falsche Eingabe!");
+            }
+
+            System.out.println("und fertig!");
+            scanner.close();
+        }
+    }
+
+
